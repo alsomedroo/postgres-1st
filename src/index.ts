4fea4d3f7ab7f6806082ae7fd7de6585ab03aeaa
alsomedroo <dhruvattek@gmail.com>
2025-02-01 21:48:54 +0530

    relation in users and address tables

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,28 @@ app.use(express.json())
 
 const pgClient = new Client("postgresql://neondb_owner:npg_BrTS9YdoGW5a@ep-lucky-glade-a8jsvzdx-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
 pgClient.connect()
-
+//@ts-ignore
 app.post("/signup", async(req,res)=>{
     const username = req.body.username
     const password = req.body.password
     const email = req.body.email
+    if(!username ||!password ||!email){
+        return res.json({
+            message: "Please provide all required fields"
+        })
+    }
+    const city = req.body.city
+    const country = req.body.country
+    const street = req.body.street
+    const pincode = req.body.pincode
+     
 
     try{
-        const insertQuery = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3);'
+        const insertQuery = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;'
         const response = await pgClient.query(insertQuery,[username, password, email])
+        console.log(response)
+        const addressInsertQuery = 'INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);'
+        const addressResponse = await pgClient.query(addressInsertQuery,[city, country, street, pincode, response.rows[0].id]) 
         res.json({
             message: "You have sign up"
         })
